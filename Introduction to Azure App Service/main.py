661ca1a88fe8c43d51af3d539a495a3790336426
Brenten Dovey <chase-dovey@Brenten-Dovey-MacBook-Pro.local>
2024-02-01 17:03:53 -0600

    Adding Compute Sections

---
+++ Introduction to Azure App Service/main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import socket
+
+# Initialize flask app
+app = Flask(__name__)
+
+# Create default route for flask app
+@app.route('/')
+def hello_world():
+    hostname = socket.gethostname()
+    return f'Hello, World! This is running on host: {hostname}'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
